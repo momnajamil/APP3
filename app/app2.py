350876eb836a07f76c05d482d06cdef35407df4c
momnajamil <161143871+momnajamil@users.noreply.github.com>
2025-06-23 00:34:27 +0500

    Update app2.py

--- app/app2.py
+++ app/app2.py
@@ -3,6 +3,33 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
+st.set_page_config(page_title="Digit Generator", layout="wide")
+
+@st.cache_resource
+def load_decoder():
+    return tf.keras.models.load_model("vae_decoder.h5")
+
+decoder = load_decoder()
+
+st.title("🧠 Handwritten Digit Generator using VAE")
+st.markdown("This app uses a trained Variational Autoencoder to generate images of handwritten digits.")
+
+if st.button("Generate 5 Digits"):
+    cols = st.columns(5)
+    for i in range(5):
+        z = np.random.normal(size=(1, 2))  # Latent space: 2D
+        prediction = decoder.predict(z)
+        image = prediction.reshape(28, 28)
+
+        fig, ax = plt.subplots()
+        ax.imshow(image, cmap='gray')
+        ax.axis('off')
+        cols[i].pyplot(fig)
+import streamlit as st
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
 @st.cache_resource
 def load_decoder():
     return tf.keras.models.load_model("vae_decoder.h5")
